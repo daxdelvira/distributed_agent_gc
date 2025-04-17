2c2aec049d4866db645042f44c17cb25f81a10f7
Dax <dax@gatech.edu>
2025-04-17 03:22:28 -0400

    fix: try different localhost

--- state_server.py
+++ state_server.py
@@ -52,4 +52,4 @@ atexit.register(cleanup)
 if __name__ == '__main__':
     import sys
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
